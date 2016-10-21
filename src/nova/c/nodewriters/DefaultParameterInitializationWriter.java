6c83a0c585c81e500b9caf1cdab2adf816d83bde
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-21 02:36:42 -0700

    Fixed default value setting for closures.

--- src/nova/c/nodewriters/DefaultParameterInitializationWriter.java
+++ src/nova/c/nodewriters/DefaultParameterInitializationWriter.java
@@ -8,14 +8,46 @@ public abstract class DefaultParameterInitializationWriter extends NodeWriter
 	
 	public StringBuilder generateSourceFragment(StringBuilder builder)
 	{
-		String use = getWriter(node().parameter).generateUseOutput().toString();
+		if (node().parameter instanceof ClosureDeclaration)
+		{
+			ClosureDeclaration decl = (ClosureDeclaration)node().parameter;
+			Closure c = (Closure)decl.getDefaultValue();
+			
+			String param = getWriter(decl).generateSourceName().toString();
+			String cast = getWriter(decl).generateTypeCast().toString();
+			
+			builder.append(param).append(" = ").append(cast).append("(").append(param).append(" == 0 ? ").append(cast);
+			getWriter(c).generateSourceFunctionReference(builder).append(" : ").append(param).append(");\n");
+			
+			cast = "(void*)";
+			String ref = getWriter(decl).generateObjectReferenceIdentifier(new StringBuilder()).toString();
+			
+			builder.append(ref).append(" = ").append(cast).append("(").append(ref).append(" == 0 ? ").append(cast);
+			getWriter(c).generateClosureInstanceReference(builder).append(" : ").append(ref).append(");\n");
+			
+			String context = decl.getContextName();
+			
+			builder.append(context).append(" = ").append(cast).append("(").append(context).append(" == 0 ? ").append(cast);
+			getWriter(c).generateClosureContextReference(builder).append(" : ").append(context).append(");");
+		}
+		else
+		{
+			generateAssignment(builder, node().parameter, node().parameter.getDefaultValue());
+		}
+		
+		return builder;
+	}
+	
+	public StringBuilder generateAssignment(StringBuilder builder, Value param, Value defaultValue)
+	{
+		String use = getWriter(param).generateUseOutput().toString();
 		
 		builder.append(use).append(" = ");
-		getWriter(node().parameter).generateTypeCast(builder).append('(');
+		getWriter(param).generateTypeCast(builder).append('(');
 		
 		builder.append(use).append(" == ");
 		
-		if (node().parameter.isPrimitive())
+		if (param.isPrimitive())
 		{
 			builder.append("(intptr_t)nova_null");
 		}
@@ -26,9 +58,7 @@ public abstract class DefaultParameterInitializationWriter extends NodeWriter
 		
 		ClassDeclaration obj = node().getProgram().getClassDeclaration("nova/Object");
 		
-		String cast = !node().parameter.getDefaultValue().isPrimitive() ? getWriter(obj).generateTypeCast().toString() : "";
-		
-		Value defaultValue = node().parameter.getDefaultValue();
+		String cast = !defaultValue.isPrimitive() ? getWriter(obj).generateTypeCast().toString() : "";
 		
 		builder.append(" ? ").append(cast).append(getWriter(defaultValue).generateSourceFragment()).append(" : ").append(cast).append(use);
 		
