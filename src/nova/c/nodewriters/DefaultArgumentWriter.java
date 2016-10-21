6dde0e85543092312798ac9ae2a376c2097ff9c9
BSteffaniak <BradenSteffaniak@gmail.com>
2016-10-21 02:36:11 -0700

    Fixed for ClosureDeclarations.

--- src/nova/c/nodewriters/DefaultArgumentWriter.java
+++ src/nova/c/nodewriters/DefaultArgumentWriter.java
@@ -1,10 +1,31 @@
 package nova.c.nodewriters;
 
+import net.fathomsoft.nova.tree.ClosureDeclaration;
 import net.fathomsoft.nova.tree.DefaultArgument;
+import net.fathomsoft.nova.tree.Value;
 
 public abstract class DefaultArgumentWriter extends IValueWriter
 {
 	public abstract DefaultArgument node();
 	
-	
+	public static StringBuilder generateDefaultArgumentOutput(StringBuilder builder, Value parameter)
+	{
+		if (parameter instanceof ClosureDeclaration)
+		{
+			builder.append("0, 0, 0");
+		}
+		else
+		{
+			if (parameter.isPrimitive())
+			{
+				builder.append("(intptr_t)nova_null");
+			}
+			else
+			{
+				builder.append(0);
+			}
+		}
+		
+		return builder;
+	}
 }
