166fd920824f33b2ef44c8b9dfeb7e85fe032f97
BSteffaniak <BradenSteffaniak@gmail.com>
2016-11-01 19:00:36 -0700

    Updated to not generate vtable instance assignment function.

--- src/nova/c/nodewriters/ExtensionDeclarationWriter.java
+++ src/nova/c/nodewriters/ExtensionDeclarationWriter.java
@@ -1,6 +1,7 @@
 package nova.c.nodewriters;
 
 import net.fathomsoft.nova.tree.ExtensionDeclaration;
+import net.fathomsoft.nova.tree.NovaMethodDeclaration;
 
 public abstract class ExtensionDeclarationWriter extends ClassDeclarationWriter
 {
@@ -9,5 +10,11 @@ public abstract class ExtensionDeclarationWriter extends ClassDeclarationWriter
 	public StringBuilder generateSource(StringBuilder builder)
 	{
 		return super.generateSource(builder);
+	}
+	
+	@Override
+	public StringBuilder generateVTableClassInstanceAssignment(StringBuilder builder, NovaMethodDeclaration method)
+	{
+		return builder;
 	}
 }
