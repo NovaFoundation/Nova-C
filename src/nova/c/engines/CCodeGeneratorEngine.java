966506644901a8a8d542999e7aa543e8e5bcca76
BSteffaniak <BradenSteffaniak@gmail.com>
2016-11-07 06:10:57 -0800

    Updated general exception message.

--- src/nova/c/engines/CCodeGeneratorEngine.java
+++ src/nova/c/engines/CCodeGeneratorEngine.java
@@ -643,8 +643,13 @@ public class CCodeGeneratorEngine extends CodeGeneratorEngine
 			mainMethodText.append	('}').append('\n');
 			mainMethodText.append	("CATCH (1)").append('\n');
 			mainMethodText.append	('{').append('\n');
+			mainMethodText.append		("char* message = \"Exception in Thread 'main'\";").append('\n');
 			mainMethodText.append		("nova_exception_Nova_Exception* base = (nova_exception_Nova_Exception*)").append(Exception.EXCEPTION_DATA_IDENTIFIER).append("->nova_exception_Nova_ExceptionData_Nova_thrownException;").append('\n');
-			mainMethodText.append		("printf(\"Exception in Thread 'main': %s\", base->nova_exception_Nova_Exception_Nova_message->nova_Nova_String_Nova_chars->nova_datastruct_list_Nova_Array_Nova_data);").append('\n');
+			mainMethodText.append		("if (base != 0 && base->nova_exception_Nova_Exception_Nova_message != 0 && base->nova_exception_Nova_Exception_Nova_message != (nova_Nova_String*)nova_null) {").append('\n');
+			mainMethodText.append			("printf(\"%s: %s\", message, base->nova_exception_Nova_Exception_Nova_message->nova_Nova_String_Nova_chars->nova_datastruct_list_Nova_Array_Nova_data);").append('\n');
+			mainMethodText.append		("} else {").append('\n');
+			mainMethodText.append			("printf(message);").append('\n');
+			mainMethodText.append		("}").append('\n');
 			mainMethodText.append		(getWriter(enter).generateSource()).append('\n');
 			mainMethodText.append	('}').append('\n');
 			mainMethodText.append	("FINALLY").append('\n');
