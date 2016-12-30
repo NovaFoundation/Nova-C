9827b8b67af9f25ead79741b62bdf0e42395d281
Braden Steffaniak <BradenSteffaniak@gmail.com>
2016-12-30 08:21:52 -0800

    Moved "precompiled" code into nova header.

--- include/Nova.h
+++ include/Nova.h
@@ -1,7 +1,41 @@
 #ifndef NOVA_NOVA
 #define NOVA_NOVA
 
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/timeb.h>
+#include <time.h>
+
+#if defined(_WIN32)
+#	ifdef _MSC_VER
+//#		define _CRT_SECURE_NO_WARNINGS
+#	endif
+#	ifndef WIN32_LEAN_AND_MEAN
+#		define WIN32_LEAN_AND_MEAN
+#	endif
+#	include <windows.h>
+#else
+#	include <pthread.h>
+#	include <unistd.h>
+#endif
+
+#ifdef USE_GC
+//#	define PARALLEL_MARK
+#	define THREAD_LOCAL_ALLOC
+#	ifdef _WIN32
+#		define GC_THREADS
+#	else
+#		define GC_PTHREADS
+#	endif
+#	include <gc.h>
+#endif
+
 #include <MacroLib.h>
+#include <CClass.h>
 
 //#define nova_Int long
 //#define nova_Long long long
@@ -17,6 +51,10 @@
 #define PCRE2_CODE_UNIT_WIDTH 8
 
 #include <stdio.h>
+#include <VTableDeclarations.h>
+// #include <InterfaceVTable.h>
+// #include <ExceptionHandler.h>
+#include <NovaClassData.h>
 
 //#include <nova/primitive/nova_primitive_Nova_Null.h>
 //#include <nova/exception/nova_exception_Nova_ExceptionData.h>
