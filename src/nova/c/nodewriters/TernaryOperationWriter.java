a3993e9bbcdf09c6a2f19ea604655bc6c5220d95
BSteffaniak <BradenSteffaniak@gmail.com>
2017-01-20 03:44:06 -0800

    Updated casting checks.

--- src/nova/c/nodewriters/TernaryOperationWriter.java
+++ src/nova/c/nodewriters/TernaryOperationWriter.java
@@ -1,6 +1,7 @@
 package nova.c.nodewriters;
 
 import net.fathomsoft.nova.tree.*;
+import net.fathomsoft.nova.tree.generics.GenericTypeArgument;
 
 public abstract class TernaryOperationWriter extends IValueWriter implements AccessibleWriter
 {
@@ -14,26 +15,60 @@ public abstract class TernaryOperationWriter extends IValueWriter implements Acc
 		String trueValue = getWriter(t).generateSourceFragment().toString();
 		String falseValue = getWriter(f).generateSourceFragment().toString();
 		
-		ClassDeclaration trueType = t.getReturnedNode().getTypeClass();
-		ClassDeclaration falseType = f.getReturnedNode().getTypeClass();
+		Value trueNode = t.getReturnedNode();
+		Value falseNode = f.getReturnedNode();
+		
+		ClassDeclaration trueType = null;
+		ClassDeclaration falseType = null;
+		
+		if (trueNode instanceof MethodCall)
+		{
+			trueNode = (Value)((MethodCall)trueNode).getCallableMethodBase();
+		}
+		if (falseNode instanceof MethodCall)
+		{
+			falseNode = (Value)((MethodCall)falseNode).getCallableMethodBase();
+		}
+		
+		trueType = trueNode.getTypeClass();
+		falseType = falseNode.getTypeClass();
 		
 		if (trueType != falseType)
 		{
-			ClassDeclaration commonClass = trueType.getCommonAncestor(f.getReturnedNode().getTypeClass());
+			ClassDeclaration commonClass = trueType.getCommonAncestor(falseNode.getTypeClass());
+			Value castNode;
 			
 			if (trueType != commonClass)
 			{
-				Value r = f.getReturnedNode();
-				
-				trueValue = getWriter(r).generateTypeCast() + trueValue;
+				castNode = falseNode;
 			}
 			else
 			{
-				Value r = t.getReturnedNode();
-				
-				falseValue = getWriter(r).generateTypeCast() + falseValue;
+				castNode = trueNode;
 			}
+			
+			trueValue = getWriter(castNode).generateTypeCast() + trueValue;
+			falseValue = getWriter(castNode).generateTypeCast() + falseValue;
 		}
+		
+//		if (trueNode.isGenericType())
+//		{
+//			GenericTypeArgument arg = trueNode.getGenericTypeParameter().getCorrespondingArgument(trueNode);
+//			
+//			if (arg != null && !arg.isGenericType())
+//			{
+//				trueValue = getWriter(arg).generateTypeCast() + trueValue;
+//			}
+//		}
+//		if (falseNode.isGenericType())
+//		{
+//			GenericTypeArgument arg = falseNode.getGenericTypeParameter().getCorrespondingArgument(falseNode);
+//			
+//			if (arg != null && !arg.isGenericType())
+//			{
+//				falseValue = getWriter(arg).generateTypeCast() + falseValue;
+//			}
+//		}
 		
 		Value condition = node().getCondition();
 		
