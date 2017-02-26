706c98ae89693653f02afbd8850480c79d8ba8a5
BSteffaniak <BradenSteffaniak@gmail.com>
2017-02-26 00:04:11 -0800

    Updated to use __thread modifier

--- src/nova/c/nodewriters/FieldDeclarationWriter.java
+++ src/nova/c/nodewriters/FieldDeclarationWriter.java
@@ -1,5 +1,6 @@
 package nova.c.nodewriters;
 
+import net.fathomsoft.nova.tree.annotations.ThreadLocalAnnotation;
 import net.fathomsoft.nova.tree.variables.FieldDeclaration;
 
 public abstract class FieldDeclarationWriter extends InstanceDeclarationWriter
@@ -11,6 +12,10 @@ public abstract class FieldDeclarationWriter extends InstanceDeclarationWriter
 		if (node().isStatic() && (node().getVisibility() == FieldDeclaration.PUBLIC || node().getVisibility() == FieldDeclaration.VISIBLE))
 		{
 			builder.append("extern ");
+		}
+		if (node().containsAnnotationOfType(ThreadLocalAnnotation.class, false, false))
+		{
+			builder.append("__thread ");
 		}
 		
 		return generateSource(builder);
