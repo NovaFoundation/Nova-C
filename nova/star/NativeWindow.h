a927f93814f514c72ba14517b8c9ca4d8a40e2e1
BSteffaniak <BradenSteffaniak@gmail.com>
2017-03-06 13:55:21 -0800

    Updated to take addedFunc

--- nova/star/NativeWindow.h
+++ nova/star/NativeWindow.h
@@ -7,18 +7,16 @@
 #include <stdlib.h>
 
 #ifdef _WIN32
-#	include <windows.h>
-
 #	define WINDOW_ID_TYPE HWND
 #else
 #   define WINDOW_ID_TYPE void*
 #endif
 
 #ifdef _WIN32
-typedef void (*nova_star_window_paint_function)(void*, void*);
+typedef void (*nova_star_window_function)(void*, void*);
 #endif
 
-WINDOW_ID_TYPE nova_createWindow(nova_star_Nova_Window* window, nova_funcStruct* paintFunc);
+WINDOW_ID_TYPE nova_createWindow(nova_star_Nova_Window* window, nova_funcStruct* paintFunc, nova_funcStruct* addedFunc);
 
 void GetDesktopResolution(int* horizontal, int* vertical);
 
