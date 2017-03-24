21bb0d2dcff1acabfa66a39314942c8ce868b6e0
BSteffaniak <BradenSteffaniak@gmail.com>
2017-03-24 14:10:01 -0700

    Updated to pass context to calling_stacktrace functions

--- include/NovaExceptionHandling.h
+++ include/NovaExceptionHandling.h
@@ -14,7 +14,8 @@ void setProgramName(char* name);
 char* addr2line(char const * const program_name, void const * const addr);
 NOVA_CODE_CONTEXT* nova_get_context();
 void nova_print_stacktrace(CONTEXT* context);
-void nova_print_calling_stacktrace(int count);
+void nova_print_calling_stacktrace(int count, NOVA_CODE_CONTEXT* context);
+char** nova_get_calling_stacktrace(int count, NOVA_CODE_CONTEXT* context, int* lines);
 LONG WINAPI nova_exception_handler(EXCEPTION_POINTERS * ExceptionInfo);
 void nova_signal_handler(int code);
 
